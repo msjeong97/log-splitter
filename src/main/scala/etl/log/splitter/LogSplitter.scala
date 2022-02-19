82546616752672fc0a053d0fd4c46671de94a002
Minseop Jeong <hpmltsws@gmail.com>
2022-02-20 02:53:13 +0900

    init: mediaLogLoader and userLogLoader

--- src/main/scala/etl/log/splitter/LogSplitter.scala
+++ src/main/scala/etl/log/splitter/LogSplitter.scala
@@ -3,12 +3,16 @@ package etl.log.splitter
 import org.apache.spark.sql.DataFrame
 import etl.common.SplittedLogDFs
 import etl.log.loader.ClientLogLoader
+import etl.log.loader.MediaLogLoader
+import etl.log.loader.UserLogLoader
 
 
 object LogSplitter {
   def splitLog(df: DataFrame): SplittedLogDFs = {
     val clientLogDF = ClientLogLoader.parse(df)
+    val mediaLogDF = MediaLogLoader.parse(df)
+    val userLogDF = UserLogLoader.parse(df)
 
-    SplittedLogDFs(clientLogDF, df, df)
+    SplittedLogDFs(clientLogDF, mediaLogDF, userLogDF)
   }
 }

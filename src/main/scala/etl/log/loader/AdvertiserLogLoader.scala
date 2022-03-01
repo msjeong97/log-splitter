3e6f9072f9c3ed91cd450469fce3a2aa9acdb1cc
Minseop Jeong <hpmltsws@gmail.com>
2022-03-01 13:42:34 +0900

    feature: implement advertiser log loader to cast essential columns to original type

--- src/main/scala/etl/log/loader/AdvertiserLogLoader.scala
+++ src/main/scala/etl/log/loader/AdvertiserLogLoader.scala
@@ -1,11 +1,26 @@
 package etl.log.loader
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, from_unixtime}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StringType, TimestampType}
 
 
 object AdvertiserLogLoader extends LogLoaderBase {
   override def parse(df: DataFrame): DataFrame = {
 
-    df
+    df.withColumn("client_id", col("client_id").cast(IntegerType))
+      .withColumn("campaign_id", col("campaign_id").cast(StringType))
+      .withColumn("action", col("action").cast(StringType))
+      .withColumn("tzoffset", col("tzoffset_c").cast(IntegerType))
+      .withColumn(
+        "utc_time",
+        from_unixtime(col("timestamp").cast(LongType) / 1000)
+          .cast(TimestampType))
+      .withColumn(
+        "local_time",
+        from_unixtime((col("timestamp").cast(LongType) / 1000) + (col("tzoffset") * 60))
+          .cast(TimestampType))
+      .withColumn("bid_price", col("bid_price").cast(DoubleType))
+      .withColumn("win_price", col("win_price").cast(DoubleType))
   }
 }

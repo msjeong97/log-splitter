40cf53ae5c7100980e2d84f70a0e0c54f0949307
Minseop Jeong <hpmltsws@gmail.com>
2022-03-12 23:55:38 +0900

    fix: to get values from map correctly

--- src/main/scala/etl/log/loader/UserLogLoader.scala
+++ src/main/scala/etl/log/loader/UserLogLoader.scala
@@ -1,16 +1,16 @@
 package etl.log.loader
 
 import eu.bitwalker.useragentutils.UserAgent
-import org.apache.spark.sql.{Column, DataFrame, Row, SparkSession}
-import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.{DataFrame}
+import org.apache.spark.sql.functions.{col, element_at, udf}
 import org.apache.spark.sql.types.{IntegerType, StringType}
-
 
 
 object UserLogLoader extends LogLoaderBase {
   override def parse(df: DataFrame): DataFrame = {
 
     val parseUserAgent = udf((uaString: String) => {
+
       val ua = new UserAgent(uaString)
       val browser = ua.getBrowser.toString
       val os = ua.getOperatingSystem.toString
@@ -19,16 +19,15 @@ object UserLogLoader extends LogLoaderBase {
       Map("browser" -> browser, "os" -> os, "device" -> device)
     })
 
-
     val loadedDf =
       df.withColumn("city", col("city").cast(StringType))
         .withColumn("county", col("county").cast(StringType))
         .withColumn("ip", col("ip").cast(StringType))
         .withColumn("user_group", col("user_group").cast(IntegerType))
-        .withColumn("os", parseUserAgent(col("ua")).getItem("os"))
-        .withColumn("device", parseUserAgent(col("ua")).getItem("device"))
-        .withColumn("browser", parseUserAgent(col("ua")).getItem("browser"))
-
+        .withColumn("ua_map", parseUserAgent(col("ua")))
+        .withColumn("os", element_at(col("ua_map"), "os"))
+        .withColumn("browser", element_at(col("ua_map"), "browser"))
+        .withColumn("device", element_at(col("ua_map"), "device"))
 
     loadedDf.select("city", "county", "ip", "user_group", "os", "device", "browser")
   }
